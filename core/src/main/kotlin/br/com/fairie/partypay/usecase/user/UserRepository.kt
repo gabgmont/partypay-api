c6d4e0dda122eb07327a252c6ed003c896285dc9
gabgmont <gabgmont>
2021-09-06 19:21:12 -0300

    fix(dependency): v0.0.1
    Fixes core module dependencies.

--- core/src/main/kotlin/br/com/fairie/partypay/usecase/user/UserRepository.kt
+++ core/src/main/kotlin/br/com/fairie/partypay/usecase/user/UserRepository.kt
@@ -2,7 +2,6 @@ package br.com.fairie.partypay.usecase.user
 
 import br.com.fairie.partypay.usecase.user.entity.User
 import br.com.fairie.partypay.vo.CPF
-import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository{
 
