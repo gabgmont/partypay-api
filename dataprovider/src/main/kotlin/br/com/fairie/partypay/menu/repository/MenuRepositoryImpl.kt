3f58d56f20cc25e7aa0096a81a8dfc2e351e2004
gabgmont <gabgmont>
2021-09-08 21:21:11 -0300

    feat(test): v0.0.1
    Implements Menu repository tests

--- dataprovider/src/main/kotlin/br/com/fairie/partypay/menu/repository/MenuRepositoryImpl.kt
+++ dataprovider/src/main/kotlin/br/com/fairie/partypay/menu/repository/MenuRepositoryImpl.kt
@@ -8,32 +8,43 @@ import br.com.fairie.partypay.usecase.menu.entity.Category
 import br.com.fairie.partypay.usecase.menu.entity.Menu
 import br.com.fairie.partypay.usecase.menu.entity.Order
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import org.springframework.util.ResourceUtils
+import java.io.File
+import java.io.FileNotFoundException
 import java.nio.file.Files
 
 class MenuRepositoryImpl : MenuRepository {
 
+    companion object{
+        const val MENU_NOT_FOUND = "Menu not found."
+        const val RESTAURANT_NOT_FOUND = "Restaurant not found."
+        const val CATEGORY_NOT_FOUND = ""
+        const val ORDER_NOT_FOUND = ""
+    }
+
     override fun getMenuByName(restaurant: String): Menu {
         val dao = getMenusFromResource()
+
         if (dao.restaurant.trim().lowercase() == restaurant.trim().lowercase())
             return dao.toVo()
-        else throw NotFoundException("Menu not found.")
+        else throw NotFoundException(MENU_NOT_FOUND)
     }
 
     override fun getCategoryByName(restaurant: String, category: String): Category {
         val menuDao = getMenusFromResource()
+
         if (menuDao.restaurant.trim().lowercase() == restaurant.trim().lowercase()) {
             menuDao.menu.forEach { categoryDao ->
                 if (categoryDao.name.trim().lowercase() == category.trim().lowercase()) {
                     return categoryDao.toVo()
                 }
             }
-            throw NotFoundException("Category not found.")
-        } else throw NotFoundException("Restaurant not found.")
+            throw NotFoundException(CATEGORY_NOT_FOUND)
+        } else throw NotFoundException(RESTAURANT_NOT_FOUND)
     }
 
     override fun getOrderByName(restaurant: String, order: String): Order {
         val menuDao = getMenusFromResource()
+
         if (menuDao.restaurant.trim().lowercase() == restaurant.trim().lowercase()) {
             menuDao.menu.forEach { categoryDao ->
                 categoryDao.orders.forEach { orderDao ->
@@ -41,13 +52,19 @@ class MenuRepositoryImpl : MenuRepository {
                         return orderDao.toVo()
                 }
             }
-            throw NotFoundException("Category not found.")
-        } else throw NotFoundException("Restaurant not found.")
+            throw NotFoundException(ORDER_NOT_FOUND)
+        } else throw NotFoundException(RESTAURANT_NOT_FOUND)
+    }
+
+    private fun getMenusFromResource(): MenuDao {
+        val url = javaClass.classLoader.getResource("menu.json") ?: throw FileNotFoundException("File Not Found.")
+        val file = File(url.file).toPath()
+
+        val content = String(Files.readAllBytes(file))
+        return jacksonObjectMapper().readValue(content, MenuDao::class.java)
     }
 }
 
-private fun getMenusFromResource(): MenuDao {
-    val content =
-        String(Files.readAllBytes(ResourceUtils.getFile("dataprovider/src/main/resources/menu.json").toPath()))
-    return jacksonObjectMapper().readValue(content, MenuDao::class.java)
-}
+
+
+
