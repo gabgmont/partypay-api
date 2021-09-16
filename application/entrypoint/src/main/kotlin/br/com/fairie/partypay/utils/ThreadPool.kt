caab7772e8672e3dbc831ae1e4efa4d25f7ba4db
lucas_galo <lucas_d>
2021-09-16 17:56:09 -0300

    feat(thread): v0.0.1
    Adds configureThreadPool private function

--- application/entrypoint/src/main/kotlin/br/com/fairie/partypay/utils/ThreadPool.kt
+++ application/entrypoint/src/main/kotlin/br/com/fairie/partypay/utils/ThreadPool.kt
@@ -8,9 +8,13 @@ class ThreadPool(
 ) {
 
     init {
-        executor.corePoolSize = 5
-        executor.maximumPoolSize = 100
-        executor.allowCoreThreadTimeOut(true)
-        executor.setKeepAliveTime(30, TimeUnit.SECONDS)
+        configureThreadPool()
+    }
+
+    private fun configureThreadPool() {
+        this.executor.corePoolSize = 5
+        this.executor.maximumPoolSize = 100
+        this.executor.allowCoreThreadTimeOut(true)
+        this.executor.setKeepAliveTime(30, TimeUnit.SECONDS)
     }
 }
